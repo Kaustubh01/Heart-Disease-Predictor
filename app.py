19dfb34cfaad693c34ce1d12a49fed3a9eec4b53
Kaustubh <kaustubhmayekar02@gmal.com>
2024-03-16 14:03:36 +0530

    Form created

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import numpy as np
 import pickle
 
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('clf.pkl', 'rb'))
 app = Flask(__name__)
 
 
@@ -11,17 +11,31 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict_disease():
-    age = int(request.form.get('age'))
-    sex = int(request.form.get('sex'))
-    blood_pressure = int(request.form.get('blood-pressure'))
-    cholestrol = int(request.form.get('cholestrol'))
-    blood_sugar = int(request.form.get('blood-sugar'))
-    ecg = int(request.form.get('ecg'))
-    print(age, sex, blood_pressure, blood_sugar, cholestrol, ecg)
-    result = model.predict(np.array([age, sex, blood_pressure, cholestrol, blood_sugar, ecg]).reshape(1, -1))
-    return str(result)
+    if request.method == "POST":
+        age = int(request.args.get('age'))
+        gender = int(request.args.get('gender'))
+        cp = int(request.args.get('cp'))
+        trestbps = int(request.args.get('trestbps'))
+        chol = int(request.args.get('chol'))
+        fasting_blood_sugar = int(request.args.get('fasting_blood_sugar'))
+        ecg = int(request.args.get('ecg'))
+        thalach = int(request.args.get('thalach'))
+        exang = int(request.args.get('exang'))
+        oldpeak = float(request.args.get('oldpeak'))
+        slope = int(request.args.get('slope'))
+        ca = int(request.args.get('ca'))
+        thal = int(request.args.get('thal'))
+
+        data = [age, gender, cp, trestbps, chol, fasting_blood_sugar, ecg, thalach, exang, oldpeak, slope, ca, thal]
+        print(data)
+        predict(np.array([data]).reshape(-1, 13))
+    return render_template('predictor.html')
+
+
+def predict(data):
+    return loaded_model.predict(data)
 
 
 if __name__ == '__main__':
