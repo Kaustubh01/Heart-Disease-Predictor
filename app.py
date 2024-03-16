4e65c716f2880e954277d721b53074cce57367e3
Kaustubh <kaustubhmayekar02@gmal.com>
2024-03-16 14:43:53 +0530

    Form formated

--- app.py
+++ app.py
@@ -14,28 +14,24 @@ def index():
 @app.route('/predict', methods=['POST', 'GET'])
 def predict_disease():
     if request.method == "POST":
-        age = int(request.args.get('age'))
-        gender = int(request.args.get('gender'))
-        cp = int(request.args.get('cp'))
-        trestbps = int(request.args.get('trestbps'))
-        chol = int(request.args.get('chol'))
-        fasting_blood_sugar = int(request.args.get('fasting_blood_sugar'))
-        ecg = int(request.args.get('ecg'))
-        thalach = int(request.args.get('thalach'))
-        exang = int(request.args.get('exang'))
-        oldpeak = float(request.args.get('oldpeak'))
-        slope = int(request.args.get('slope'))
-        ca = int(request.args.get('ca'))
-        thal = int(request.args.get('thal'))
+        age = int(request.form.get('age'))
+        gender = int(request.form.get('gender'))
+        cp = int(request.form.get('cp'))
+        trestbps = int(request.form.get('trestbps'))
+        chol = int(request.form.get('chol'))
+        fasting_blood_sugar = int(request.form.get('fasting_blood_sugar'))
+        ecg = int(request.form.get('ecg'))
+        thalach = int(request.form.get('thalach'))
+        exang = int(request.form.get('exang'))
+        oldpeak = float(request.form.get('oldpeak'))
+        slope = int(request.form.get('slope'))
+        ca = int(request.form.get('ca'))
+        thal = int(request.form.get('thal'))
 
         data = [age, gender, cp, trestbps, chol, fasting_blood_sugar, ecg, thalach, exang, oldpeak, slope, ca, thal]
         print(data)
-        predict(np.array([data]).reshape(-1, 13))
+        return model.predict(np.array([200, 0, 0, 125, 212, 0, 1, 168, 0, 1.0, 2, 2, 3]))
     return render_template('predictor.html')
-
-
-def predict(data):
-    return loaded_model.predict(data)
 
 
 if __name__ == '__main__':
